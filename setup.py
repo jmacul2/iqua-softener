43f900b16bb0265c282cfe3fa8365f13ace96ca3
Jay McEntire <599790+jmacul2@users.noreply.github.com>
2025-11-09 18:38:13 -0700

    updated setup to latest version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iqua_softener",
-    version="2.0.0",
+    version="2.0.1",
     license="MIT",
     author="Artur Zabroński",
     author_email="artur.zabronski@gmail.com",
